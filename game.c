e1fa63fef7aa8d2bb23664401f1bec1e0ac3a256
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-01-02 16:49:07 +0200

    initial commit

---
+++ game.c
@@ -0,0 +1,193 @@
+/* Game of life */
+/* Implementation using local variables only */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void load_world_from_file(char *filename, int *worldX, int *worldY, char ***world, char ***next_world);
+void save_world_to_file(char *filename, int worldX, int worldY, char **world);
+void next_generation(int worldX, int worldY, char ***world, char ***next_world);
+int get_next_state(int x, int y, int worldX, int worldY, char **world);
+int count_neighbors(int x, int y, int worldX, int worldY, char **world);
+void unload_world(int worldX, char **word, char **next_world);
+
+int main(int argc, char **argv) {
+    int i;
+
+    /* the current world and the world after one generation */
+    char **world, **next_world;
+
+    /* fixed x,y dimensions of the worlds */
+    int worldX, worldY;
+
+    if (argc != 3) {
+        printf("./a.out input_file output_file\n");
+        return 0;
+    }
+    
+    load_world_from_file(argv[1], &worldX, &worldY, &world, &next_world);
+
+    /* advance world by 50 generations */
+    for (i = 0; i < 50; i++) {
+        next_generation(worldX, worldY, &world, &next_world);
+    }
+
+    save_world_to_file(argv[2], worldX, worldY, world);
+
+    unload_world(worldX, world, next_world);
+
+    return 0;
+}
+
+/* Reads a world from the specified filename and allocates
+ * the required memory both for current and next generation worlds.
+ * 
+ * 4 additional 1-dimensional arrays are allocated for the cells
+ * that surround the world. These cells are always dead during the simulation
+ * (value = 0), as a result the world never expands
+ * outside the specified grid (indicated by the first line in each input file)
+ */
+void load_world_from_file(char *filename, int *worldX, int *worldY, char ***world, char ***next_world) {
+    FILE *fp;
+    int i, j;
+    int state;
+
+    fp = fopen(filename, "r");
+    if (fp == NULL) {
+        printf("Error opening input file %s\n", filename);
+        exit(-1);
+    }
+
+    /* read world dimensions */
+    fscanf(fp, "%d %d", worldX, worldY);
+    getc(fp);
+
+    /* allocate 1st dimension of the world and next_world (includes the
+    surrounding cells */
+    *world = malloc((*worldX + 2) * sizeof(char *));
+    if (*world == NULL) {
+        printf("main alloc in current world");
+        exit(-1);
+    }
+    *next_world = malloc((*worldX + 2) * sizeof(char *));
+    if (*next_world == NULL) {
+        printf("main alloc in next world");
+        exit(-1);
+    }
+
+    /* allocate 2nd dimension of world and next_world includes the
+    surrounding cells */
+    for (i = 0; i < *worldX + 2; i++) {
+        (*world)[i] = malloc((*worldY + 2) * sizeof(char));
+        if ((*world)[i] == NULL) {
+            printf("line %d alloc in current world", i);
+            exit(-1);
+        }
+        (*next_world)[i] = malloc((*worldY + 2) * sizeof(char));
+        if ((*next_world)[i] == NULL) {
+            printf("line %d alloc in next world", i);
+            exit(-1);
+        }
+    }
+    
+    /* initialize the surrounding cells of world and next_world to 0 */
+    for (i = 0; i < *worldX + 2; i++) {
+        (*world)[i][0] = (*world)[i][*worldY + 1] =
+            (*next_world)[i][0] = (*next_world)[i][*worldY + 1] = 0;
+
+    }
+    for (j = 0; j < *worldY + 2; j++) {
+        (*world)[0][j] = (*world)[*worldX + 1][j] =
+            (*next_world)[0][j] = (*next_world)[*worldX + 1][j] = 0;
+    }
+
+    /* initialize the rest of the world from file data */
+    for (i = 1; i < *worldX + 1; i++) {
+        for (j = 1; j < *worldY + 1; j++) {
+            getc(fp);
+            (getc(fp) == '*') ? ((*world)[i][j] = 1) : ((*world)[i][j] = 0);
+        }
+        getc(fp);
+        getc(fp);
+    }
+   
+    fclose(fp);
+}
+
+/* Saves the world to the file indicated by the specified filename.
+ * The surrounding cells are not saved (they are not part of the world)
+ */
+void save_world_to_file(char *filename, int worldX, int worldY, char **world) {
+    FILE *fp;
+    int i, j;
+
+    fp = fopen(filename, "w");
+    if (fp == NULL) {
+        printf("Error opening output file");
+        exit(-1);
+    }
+
+    fprintf(fp, "%d %d\n", worldX, worldY);
+    for (i = 1; i < worldX + 1; i++) {
+        for (j = 1; j < worldY + 1; j++) {
+            putc('|', fp);
+            (world[i][j] == 1) ? putc('*', fp) : putc(' ', fp); 
+        }
+        putc('|', fp);
+        putc('\n', fp);
+    }
+    fclose(fp);
+}
+
+/* Calculates the state of cells in the next world and swaps current and
+ * next world.
+ * The surrounding cells are always dead, their state is always 0 (dead).
+ */
+void next_generation(int worldX, int worldY, char ***world, char ***next_world) {
+    int i, j;
+    char **world_tmp;
+    
+    for (i = 1; i < worldX + 1; i++) {
+        for (j = 1; j < worldY + 1; j++) {
+            (*next_world)[i][j] = get_next_state(i, j, worldX, worldY, *world);
+        }
+    }
+    world_tmp = *world;
+    *world = *next_world;
+    *next_world = world_tmp;
+}
+
+/* Calculates and returns the new state of a single cell.
+ * Returns 0 if in the new world the cell is dead, 1 otherwise.
+ */
+int get_next_state(int x, int y, int worldX, int worldY, char **world) {
+    int neightbours = count_neighbors(x, y, worldX, worldY, world);
+
+    return (neightbours == 3 || (neightbours == 2 && world[x][y] == 1)) ? 1 : 0;
+}
+
+/* Returns the number of neighbors of a single cell that is not a
+ * surrounding cell of the world.
+ */
+int count_neighbors(int x, int y, int worldX, int worldY, char **world) {
+    if (x <= 0 || y <= 0 || x >= worldX + 1 || y >= worldY + 1) {
+        printf("(%d, %d) does not have 8 neighbors\n", x, y);
+        exit(-1);
+    }
+    return world[x - 1][y] + world[x + 1][y] + world[x][y - 1]
+            + world[x][y + 1] + world[x - 1][y - 1] + world[x + 1][y + 1]
+            + world[x + 1][y - 1] + world[x - 1][y + 1];
+}
+
+/* Frees the allocated memory for both current and next world.
+ */
+void unload_world(int worldX, char **world, char **next_world) {
+    int i;
+
+    for (i = 0; i < worldX + 2; i++) {
+        free(world[i]);
+        free(next_world[i]);
+    }
+    free(world);
+    free(next_world);
+}
